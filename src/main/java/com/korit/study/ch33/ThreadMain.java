6d327513ad739f4f498f1c23da0f1334de44e76d
qkrwogus202 <cqfq4rgh4x@privaterelay.appleid.com>
2025-10-30 17:15:58 +0900

    feat: 쓰레드

--- src/main/java/com/korit/study/ch33/ThreadMain.java
+++ src/main/java/com/korit/study/ch33/ThreadMain.java
@@ -1,30 +1,62 @@
 package com.korit.study.ch33;
 
+class Counter{
+
+    private int count = 0;
+
+    public synchronized  void increment() {
+        count++;
+    }
+    public synchronized void decrement() {
+        count --;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
+
 public class ThreadMain {
     public static void main(String[] args) {
 
-        int sum = 0;
+
+
+        Counter counter = new Counter();
 
         Thread thread1 = new Thread(() -> {
             for (int i = 0; i < 10; i ++) {
                 System.out.println("1번 반복문: " + i);
-                sum += i;
+                counter.increment();
             }
         });
 
         Thread thread2 = new Thread(() -> {
             for (int i = 0; i < 10; i ++) {
                 System.out.println("2번 반복문: " + i);
-                sum += i;
+                counter.increment();
             }
         });
+
+
 
         System.out.println("쓰레드 시작!!!");
 
         thread1.start();
         thread2.start();
 
-        System.out.println("쓰레드 종료!!!");
+        try {
+            thread1.join();
+            thread2.join();
+
+            System.out.println(counter.getCount());
+            System.out.println("쓰레드 종료!!!");
+
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
 
 
 
